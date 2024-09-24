4fab9f4e9ec72e480450d4c7ea89ebb0534cea84
jortboy2 <vonghung849@gmail.com>
2024-09-24 14:50:10 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "./tailwind/output.css";
 import logo from "./Logo-Tikop.png";
-import point from "./burger.png";
+import burget from "./burger.png";
+import chicken from "./chicken.png";
+import boom from "./boom.png";
 import { Fragment, useEffect, useState } from "react";
 const data = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 6 }];
+const datapoint = [{ url: burget }, { url: chicken }, { url: boom }];
 function App() {
   const [show, setShow] = useState(true);
   const [points, setPoint] = useState(0);
   const [notify, setNotify] = useState();
   const [RandomNumer, setRandomNumer] = useState();
+  const [RandomImage, setRandomImage] = useState(1);
+  const [countSeconds, setCountSeconds] = useState(30);
+  const [alertpoint, setAlertPoint] = useState();
   // true là trồi lên
   // false là trồi xuống
   // console.log(Math.round(Math.random() * data.length))
@@ -15,7 +21,8 @@ function App() {
     const inter = setInterval(
       () => {
         setShow((prev) => !prev);
-        setRandomNumer(Math.round(Math.random() * data.length));
+        setRandomNumer(Math.floor(Math.random() * data.length));
+        
       },
       show ? 3000 : 1000
     );
@@ -23,14 +30,37 @@ function App() {
       clearInterval(inter);
     };
   }, [show]);
+  const handleStart = () => {
+    const counttime = setInterval(() => {
+      setCountSeconds((prev) => prev - 1);
+    }, 1000);
+    return () => {
+      clearInterval(counttime);
+    };
+  };
+  useEffect(() => {
+    if (countSeconds <= 0) {
+      setCountSeconds(0);
+    }
+  }, [countSeconds]);
   const handlePoint = (e) => {
+    setRandomImage(Math.floor(Math.random() * datapoint.length));
     const id = e.target.getAttribute("data-id");
     console.log("RandomNumer", RandomNumer);
     console.log("id", id);
     if (!show && id == RandomNumer) {
       setNotify(RandomNumer);
-      setPoint((prev) => prev + 10);
       setShow(true);
+      if (datapoint[RandomImage] == datapoint[0]) {
+        setPoint((prev) => prev + 20);
+        setAlertPoint("+ 20 points");
+      } else if (datapoint[RandomImage] == datapoint[1]) {
+        setPoint((prev) => prev + 50);
+        setAlertPoint("+ 50 points");
+      } else if (datapoint[RandomImage] == datapoint[2]) {
+        setPoint((prev) => prev - 10);
+        setAlertPoint("- 10 points");
+      }
     } else {
       console.log("Chưa lên đã bấm r cha");
     }
@@ -43,10 +73,23 @@ function App() {
       }, 100);
     }
   });
+  useEffect(() => {
+    console.log("RandomImage", RandomImage);
+  }, [RandomImage]);
   // console.log(data.length)
   return (
     <div>
       <span className="text-[40px]">Point : {points}</span>
+      <div className="w-full text-center">
+        <span>{countSeconds}</span>
+        <br />
+        <button
+          className="py-2 px-5 rounded-full hover:bg-emerald-600 bg-emerald-200"
+          onClick={handleStart}
+        >
+          Start
+        </button>
+      </div>
       <div className="w-full h-screen grid grid-cols-3">
         {/* start */}
         {data.map((item, index) => (
@@ -59,10 +102,10 @@ function App() {
             <span>
               {notify == item.id ? (
                 <div
-                  className="text-[red] absolute top-20 opacity-100 transition-opacity duration-[1000ms]" 
+                  className="text-[red] absolute top-20 opacity-100 transition-opacity duration-[1000ms]"
                   id="fade-text"
                 >
-                  + 10 point
+                  {alertpoint}
                 </div>
               ) : (
                 <Fragment />
@@ -85,7 +128,7 @@ function App() {
                 id="point"
               >
                 <img
-                  src={point}
+                  src={datapoint[RandomImage].url}
                   alt="point"
                   className="w-full h-full object-cover"
                 />
